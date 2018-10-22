04a04a4d55b28c127d37a41707dbf13bfc7fccf4
John Yue <yh113@sina.com>
2018-10-22 21:21:30 +0800

    Create proj

---
+++ src/main/java/com/iss/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.iss.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping("/Greeting")
+	public String Greeting() {
+		return "Hello!";
+	}
+}
